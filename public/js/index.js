8b456ed3f928b907237ee5b16f78fbbabcabf96d
CryptoCoolby <ruthner97@gmail.com>
2018-08-29 16:52:42 +0200

    Add username option

--- public/js/index.js
+++ public/js/index.js
@@ -17,14 +17,15 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
     console.log('Incoming message:', message)
     let li = $('<li></li>').text(message.from + " says: " + message.text)
-    $('#messages').append(li)
+    $('#messages').prepend(li)
 })
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault()
 
     let text = $('[name=message]').val()
-    socket.emit('createMessage', {from: 'User', text}, function (data) {
+    let from = $('[name=username]').val()
+    socket.emit('createMessage', {from, text}, function (data) {
         console.log(data)
     })
 })
